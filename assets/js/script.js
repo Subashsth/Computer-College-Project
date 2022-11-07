d1e005c4af0b4f684c4f5afb2d6ca26b0951ae04
Subashsth <9819690893.subash@gmail.com>
2022-11-07 21:47:39 +0545

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,21 +44,3 @@ counters.forEach((counter) => {
     };
     updateCounter();
 })
-
-
-// var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-// var cardWidth = $('.carousel-item').width();
-
-// var scrollPosition = 0;
-// $('.carousel-control-next').on('click', function () {
-//     if (scrollPosition < (carouselWidth - (cardWidth * 3))) {
-//         scrollPosition = scrollPosition + cardWidth;
-//         $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
-//     }
-// });
-// $('.carousel-control-prev').on('click', function () {
-//     if (scrollPosition > 0) {
-//         scrollPosition = scrollPosition - cardWidth;
-//         $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
-//     }
-// });
